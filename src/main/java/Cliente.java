dbb9148bc3590c3a0dec302814d49aef3ae9b762
carloseduardoscc <carloseduscc@gmail.com>
2025-09-25 20:51:16 -0300

    feat(Conta): Implementa tranferência, saque e depósito

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public class Cliente {
+    private String nome;
+    private final int cpf;
+
+    public Cliente(int cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", cpf=" + cpf +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return cpf == cliente.cpf;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(cpf);
+    }
+}
---
+++ src/main/java/Conta.java
@@ -0,0 +1,95 @@
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Conta {
+    private static final AtomicLong SEQ = new AtomicLong(1);
+    private static int agenciaDefault = 001;
+
+    private long numero;
+    private BigDecimal saldo;
+    private final int agencia;
+    private final Cliente titular;
+
+    public Conta(int agencia, Cliente titular) {
+        this.agencia = agencia;
+        this.titular = titular;
+    }
+
+    public Conta(Cliente titular) {
+        this.titular = titular;
+        this.agencia = agenciaDefault;
+    }
+
+    public void transferir(BigDecimal quantia, Conta contaRecebedoraDoDeposito) {
+        try {
+            this.sacar(quantia);
+            contaRecebedoraDoDeposito.depositar(quantia);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Não foi possível concluir a transferência bancária: " + e.getMessage());
+        }
+    }
+
+    public void depositar(BigDecimal quantia) {
+        if (quantia == null || quantia.longValue() <= 0) {
+            throw new IllegalArgumentException("Valor de depósito não pode ser nulo, 0 ou negativo");
+        }
+        this.saldo = this.saldo.add(quantia);
+    }
+
+    public void sacar(BigDecimal quantia) {
+        if (quantia == null || quantia.signum() <= 0) {
+            throw new IllegalArgumentException("Valor de saque não pode ser nulo, 0 ou negativo");
+        }
+        if (saldo.compareTo(quantia) < 0) {
+            throw new IllegalArgumentException("Valor de saque não pode ser maior que o saldo");
+        }
+        this.saldo = this.saldo.subtract(quantia);
+    }
+
+    public static void setAgenciaDefault(int agenciaDefault) {
+        Conta.agenciaDefault = agenciaDefault;
+    }
+
+    public static int getAgenciaDefault() {
+        return agenciaDefault;
+    }
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "numero=" + numero +
+                ", saldo=" + saldo +
+                ", agencia=" + agencia +
+                ", titular=" + titular +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return numero == conta.numero && agencia == conta.agencia && Objects.equals(titular, conta.titular);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numero, agencia, titular);
+    }
+}
